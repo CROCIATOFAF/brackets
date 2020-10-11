2f59e0659ead8765a937d419fb991e017ccb706c
CROCIATOFAF <48692205+CROCIATOFAF@users.noreply.github.com>
2020-10-12 01:58:04 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brackets = bracketsConfig.flat().join('');
+    let stack = [];
+
+    for (const bracket of str) {
+        let bracketsIndex = brackets.indexOf(bracket);
+
+        if (stack.length > 0 && brackets[bracketsIndex] === brackets[stack[stack.length - 1]]) {
+            stack.pop();
+            continue;
+        } else if (bracketsIndex % 2 === 0) {
+            stack.push(bracketsIndex + 1);
+        } else {
+            if (stack.pop() !== bracketsIndex) {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
 }
